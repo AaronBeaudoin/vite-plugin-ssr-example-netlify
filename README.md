c77a205a5495dc1df6b6d418e14cc6323791d827
Aaron Beaudoin <aaron.beaudoin@aryse.com>
2022-07-26 10:42:52 -0500

    Added console log to function

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 1. Fork this repository.
 2. Go to https://app.netlify.com/start.
 3. Connect to GitHub and select your fork.
+4. Press the "Deploy" button!
--- functions/index.ts
+++ functions/index.ts
@@ -4,7 +4,8 @@ import { renderPage } from "vite-plugin-ssr";
 export const handler: Handler = async event => {
   const pageContext = await renderPage({ url: event.rawUrl });
   if (!pageContext.httpResponse) return { statusCode: 200 };
-
+  console.log(pageContext.httpResponse.statusCode, event.rawUrl);
+  
   return {
     statusCode: pageContext.httpResponse.statusCode,
     headers: { "Content-Type": pageContext.httpResponse.contentType },
